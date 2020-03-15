80cb53897b88ae98a8e99f1ea1f807a8a2022b20
Roar Selbo <dermal@selbo.net>
2020-03-15 11:52:41 +0100

    Upgraded to 1.1

--- Source/CompUseEffect_CallTrader.cs
+++ Source/CompUseEffect_CallTrader.cs
@@ -7,6 +7,18 @@ namespace TraderWhereAreYou
 {
     public class CompUseEffect_CallTrader : CompUseEffect
     {
+        public override bool CanBeUsedBy(Pawn p, out string failReason)
+        {
+            Map map = p.Map;
+            if (map.passingShipManager.passingShips.Count >= 5)
+            {
+                failReason = "MaxShipsInOrbit".Translate();
+                return false;
+            }
+
+            failReason = null;
+            return true;
+        }
         // this is a weird amalgamation between CompDoEffect and IncidentWorker_OrbitalTraderArrival
         public override void DoEffect(Pawn usedBy)
         {
@@ -17,22 +29,25 @@ namespace TraderWhereAreYou
             {
                 return;
             }
-            if ((from x in DefDatabase<TraderKindDef>.AllDefs
-                 where x.orbital
-                 select x).TryRandomElementByWeight((TraderKindDef traderDef) => traderDef.commonality, out TraderKindDef def))
+
+            if (DefDatabase<TraderKindDef>.AllDefs
+                .Where(x => x.orbital)
+                .TryRandomElementByWeight(traderDef => traderDef.commonality, out TraderKindDef def))
             {
                 TradeShip tradeShip = new TradeShip(def);
                 if (map.listerBuildings.allBuildingsColonist.Any((Building b) => b.def.IsCommsConsole && b.GetComp<CompPowerTrader>().PowerOn))
                 {
                     Find.LetterStack.ReceiveLetter(tradeShip.def.LabelCap, "TraderArrival".Translate(
                         tradeShip.name,
-                        tradeShip.def.label
+                        tradeShip.def.label,
+                        "TraderArrivalNoFaction".Translate()
                     ), LetterDefOf.PositiveEvent, null);
                 }
                 map.passingShipManager.AddShip(tradeShip);
                 tradeShip.GenerateThings();
             }
             else throw new InvalidOperationException();
+
         }
     }
 }
