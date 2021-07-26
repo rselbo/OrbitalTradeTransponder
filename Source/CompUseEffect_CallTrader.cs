e11233ce7205c2c7be2f1b1790972ac846df0c07
Roar Selbo <dermal@selbo.net>
2021-07-26 22:13:44 +0200

    Rebalanced the mod and added specific orbital beacons for different tradeship types

--- Source/CompUseEffect_CallTrader.cs
+++ Source/CompUseEffect_CallTrader.cs
@@ -1,5 +1,7 @@
 ﻿using RimWorld;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Verse;
 
@@ -7,6 +9,21 @@ namespace TraderWhereAreYou
 {
     public class CompUseEffect_CallTrader : CompUseEffect
     {
+        /// <summary>
+        /// By default props returns the base CompProperties class.
+        /// You can get props and cast it everywhere you use it, 
+        /// or you create a Getter like this, which casts once and returns it.
+        /// Careful of case sensitivity!
+        /// </summary>
+        public OrbitalCompUseEffectProperties Props => (OrbitalCompUseEffectProperties)this.props;
+
+        public string TradeBeaconType => Props.TradeBeaconType;
+
+        public override void Initialize(CompProperties props)
+        {
+            base.Initialize(props);
+            Log.Message("CompUseEffect_CallTrader::Initialize");
+        }
         public override bool CanBeUsedBy(Pawn p, out string failReason)
         {
             Map map = p.Map;
@@ -24,15 +41,27 @@ namespace TraderWhereAreYou
         {
             base.DoEffect(usedBy);
 
+            Log.Message($"[OrbitalTradeBeacon] Effect {TradeBeaconType}");
             Map map = usedBy.Map;
             if (map.passingShipManager.passingShips.Count >= 5)
             {
                 return;
             }
 
-            if (DefDatabase<TraderKindDef>.AllDefs
-                .Where(x => x.orbital)
-                .TryRandomElementByWeight(traderDef => traderDef.commonality, out TraderKindDef def))
+            TraderKindDef def = null;
+            var iDef = DefDatabase<TraderKindDef>.AllDefs.Where(x => x.orbital && x.defName == TradeBeaconType);
+            if(iDef.EnumerableNullOrEmpty())
+            {
+                if(DefDatabase<TraderKindDef>.AllDefs.Where(x => x.orbital).TryRandomElementByWeight(traderDef => traderDef.commonality, out TraderKindDef weightedDef))
+                {
+                    def = weightedDef;
+                }
+            }
+            else
+            {
+                def = iDef.First();
+            }
+            if (def != null)
             {
                 TradeShip tradeShip = new TradeShip(def);
                 if (map.listerBuildings.allBuildingsColonist.Any((Building b) => b.def.IsCommsConsole && b.GetComp<CompPowerTrader>().PowerOn))
@@ -48,6 +77,18 @@ namespace TraderWhereAreYou
             }
             else throw new InvalidOperationException();
 
+        }
+    }
+
+    public class OrbitalCompUseEffectProperties : CompProperties_UseEffect
+    {
+        public string TradeBeaconType;
+        /// <summary>
+        /// These constructors aren't strictly required if the compClass is set in the XML.
+        /// </summary>
+        public OrbitalCompUseEffectProperties()
+        {
+            this.compClass = typeof(CompUseEffect_CallTrader);
         }
     }
 }
